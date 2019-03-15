142c36b6428f01d732013fb282681a591cc17869
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2019-03-15 15:18:01 -0400

    Add new tests for Request

--- src/test/java/io/github/ved/jrequester/RequestTest.java
+++ src/test/java/io/github/ved/jrequester/RequestTest.java
@@ -1,13 +1,95 @@
 package io.github.ved.jrequester;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
 public class RequestTest {
 	
 	@Test
-	void testWeightHandling(){
+	void commandName(){
+		
+		Request request = new Request("!hi");
+		
+		assertEquals("hi", request.getCommand());
+		assertEquals("!hi", request.getCommandNoFormat());
+		
+	}
+	
+	@Test
+	void argLessLongOptions(){
+		
+		Request request = new Request("!hi --one --two --three");
+		
+		assertEquals(3, request.getOptionsData().size());
+		
+		assertTrue(request.hasOption("one"));
+		assertTrue(request.hasOption("two"));
+		assertTrue(request.hasOption("three"));
+		
+	}
+	
+	@Test
+	void argLessShortOptionsSeparated(){
+		
+		Request request = new Request("!hi -a -b -c");
+		
+		assertEquals(3, request.getOptionsData().size());
+		
+		assertTrue(request.hasOption("a"));
+		assertTrue(request.hasOption("b"));
+		assertTrue(request.hasOption("c"));
+		
+	}
+	
+	@Test
+	void argLessShortOptionsCombined(){
+		
+		Request request = new Request("!hi -abc");
+		
+		assertEquals(3, request.getOptionsData().size());
+		
+		assertTrue(request.hasOption("a"));
+		assertTrue(request.hasOption("b"));
+		assertTrue(request.hasOption("c"));
+		
+	}
+	
+	@Test
+	void argLessCombinedTypesOptions(){
+		
+		Request request1 = new Request("!hi --one --two -ab");
+		
+		assertEquals(4, request1.getOptionsData().size());
+		
+		assertTrue(request1.hasOption("one"));
+		assertTrue(request1.hasOption("two"));
+		assertTrue(request1.hasOption("a"));
+		assertTrue(request1.hasOption("b"));
+		
+		Request request2 = new Request("!hi -ab --one --two");
+		
+		assertEquals(4, request2.getOptionsData().size());
+		
+		assertTrue(request2.hasOption("one"));
+		assertTrue(request2.hasOption("two"));
+		assertTrue(request2.hasOption("a"));
+		assertTrue(request2.hasOption("b"));
+		
+		Request request3 = new Request("!hi --one -ab --two");
+		
+		assertEquals(4, request3.getOptionsData().size());
+		
+		assertTrue(request3.hasOption("one"));
+		assertTrue(request3.hasOption("two"));
+		assertTrue(request3.hasOption("a"));
+		assertTrue(request3.hasOption("b"));
+		
+	}
+	
+	@Test
+	void weightHandling(){
 		
 		Request testRequest = new Request("!test -abcd");
 		
@@ -28,7 +110,7 @@ public class RequestTest {
 	}
 	
 	@Test
-	void testWeightHandlingModifications(){
+	void weightHandlingModifications(){
 		
 		Request testRequest = new Request("!test -abcd");
 		
@@ -50,6 +132,18 @@ public class RequestTest {
 		assertEquals(1, weightForBMod);
 		assertEquals(32, weightForCMod);
 		assertEquals(2, weightForDMod);
+		
+	}
+	
+	@Test
+	void spaceInPrefix(){
+		
+		String prefix = "yo ";
+		
+		Request testRequest = new Request("yo test", prefix);
+		
+		assertEquals(prefix, testRequest.getCommandPrefix());
+		assertEquals("test", testRequest.getCommand());
 		
 	}
 	
