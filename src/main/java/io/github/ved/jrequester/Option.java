20ded3be0e00460be166285c324cb67188daa2ec
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2019-03-15 15:16:33 -0400

    Add setters for Option's properties

--- src/main/java/io/github/ved/jrequester/Option.java
+++ src/main/java/io/github/ved/jrequester/Option.java
@@ -30,44 +30,74 @@ public class Option {
 	public Option(String description, boolean acceptsContent, int weight,
 			String name, String... variants){
 		
-		this.description = description;
-		this.acceptsContent = acceptsContent;
-		this.weight = weight;
-		this.name = name;
-		this.variants = variants;
+		this.setName(name);
+		this.setDescription(description);
+		this.setAcceptsContent(acceptsContent);
+		this.setWeight(weight);
+		this.setVariants(variants);
 		
+	}
+
+	// Package protected constructor for Option Building
+	Option(){
+		this.setAcceptsContent(true);
 	}
 	
 	public String getDescription(){
 		return this.description;
 	}
 	
+	void setDescription(String description){
+		this.description = description;
+	}
+	
 	public boolean doesAcceptsContent(){
 		return this.acceptsContent;
+	}
+	
+	void setAcceptsContent(boolean acceptsContent){
+		this.acceptsContent = acceptsContent;
 	}
 	
 	public int getWeight(){
 		return this.weight;
 	}
 	
+	void setWeight(int weight){
+		this.weight = weight;
+	}
+	
 	public String getName(){
 		return this.name;
+	}
+	
+	void setName(String name){
+		if(name == null){
+			throw new IllegalArgumentException(
+					"The option's name cannot be null!");
+		}
+		
+		this.name = name;
 	}
 	
 	public String[] getVariants(){
 		return this.variants;
 	}
 	
+	void setVariants(String... variants){
+		this.variants = variants;
+	}
+	
 	public boolean has(String optionName){
-		return getAllOptions().contains(optionName);
+		return this.getAllOptions().contains(optionName);
 	}
 	
 	public List<String> getAllOptions(){
 		List<String> allOptions = new ArrayList<>();
 		
-		allOptions.add(getName());
+		allOptions.add(this.getName());
 		
-		allOptions.addAll(Arrays.asList(getVariants()));
+		allOptions.addAll(Arrays.asList(this.getVariants()));
 		
 		return allOptions;
 	}
